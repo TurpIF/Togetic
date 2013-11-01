17ddec1d7788d702b0dac0fef122e82ef5fcaafd
pierre.turpin@insa-lyon.fr <pierre.turpin@insa-lyon.fr>
2013-11-01 19:04:36 +0100

    Modifie la position d'un objet blender continuellement avec un controle multithreadé

--- Blender/PositionController.py
+++ Blender/PositionController.py
@@ -1,21 +1,23 @@
 import GameLogic
 import Rasterizer
 import threading
+import mathutils
 import time
 
 # Shared memory
-relPosition = [0, 1, 0]
+relPosition = mathutils.Vector((0, 1, 0))
 lockPosition = threading.Lock()
 
 initialized = False
-initPosition = [0, 0, 0]
+initPosition = mathutils.Vector((0, 0, 0))
 
-Sample trajectory to test the module
+# Sample trajectory to test the module
 import math
-time = 0
-path = lambda t: (10 * math.cos(t), 10 * math.sin(t), 0)
-def getPosition():
-    time += 0.1
+def setPosition(time):
+    global lockPosition
+    global relPosition
+
+    path = lambda t: mathutils.Vector((10 * math.cos(t), 10 * math.sin(t), 0))
     pos = path(time)
 
     lockPosition.acquire(True)
@@ -23,23 +25,28 @@ def getPosition():
     lockPosition.release()
 
 def server():
+    t = 0
     while True:
-        getPosition()
-        time.sleep(0.1)
+        setPosition(t)
+        time.sleep(0.01)
+        t += 0.01
 
 t = threading.Thread(target=server)
 t.start()
 
 def main():
+    global lockPosition
+    global relPosition
+    global initialized
+    global initPosition
+
     controller = GameLogic.getCurrentController()
     owner = controller.owner
 
     if not initialized:
-        initPosition = owner.worldPosition
+        initPosition = owner.worldPosition.copy()
         initialized = True
 
     lockPosition.acquire(True)
-    owner.worldPosition = map(
-        lambda (abs, rel) : abs + rel,
-        zip(initPosition, relPosition))
+    owner.worldPosition = initPosition + relPosition
     lockPosition.release()

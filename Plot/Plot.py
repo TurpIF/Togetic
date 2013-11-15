edc4f9915d3bd46dde6bd9f75076e5ef297500f0
Stefjeanne <stephane.baudrand@gmail.com>
2013-11-15 18:32:00 +0100

    Update Plot.py
    J'ai ajouté les threading.Lock() à la classe Courbe, corrigé quelques erreurs, pris en compte l'indice de la liste de données et écrit un petit test. Cela fonctionne mis à part que le programme plante quand on clique sur le graphe...

--- Plot/Plot.py
+++ Plot/Plot.py
@@ -1,90 +1,110 @@
 import socket
 from numpy import *
 import pylab
-import Tkinter as Tk
+#import Tkinter as Tk
 from Server import Server
 import threading
+import time
+     
+### C'est une mémoire partagé. Il faut un threading.Lock associé pour protéger les accès.
+### Pour l'instant tu utilises une simple liste. Au début reste comme ça.
+### Une fois que ça fonctionne, essaye de transformer ta liste en Queue. C'est fait exprès pour les threads.
+### Seulement une fois que ça fonctionne
 
 global data  #Tableau de données
-
+lock = threading.Lock()
+     
 class Client(Server):
-	connexion_to_server=0
+            
 	def __init__ (self):
 		Server.__init__(self)
+        ### Utilise le fichier Server.py que j'ai mis dans le dossier Blender. Je le modifie cette aprem (jeudi aprem).
+        ### Tu peux t'inspirer du code de PositionController.py qui utilise ce fichier.
+        ### Copie le et utilise la version que tu as. Si tu travailles dessus avant que je modifie, c'est pas trop grave.
+        ### Il faudra juste readapter le code. Mais c'est pas compliquer
 		connexion_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     
 	def connect(self, addr, port):
 		connexion_to_server.connect((addr, port))
-			
+                           
+     
+class Courbe(threading.Thread):
+	              
+        def __init__(self, grandeur):
 
-class Courbe:
-	t=0                # Temps actuel
-	delta_t=0          # Pas de temps
-	retrace=0          # Retrace le graphique à chaque retrace millisecondes
-	temps, valeur = 0,0   # Vecteurs temps et valeur de fonction pour graphique
-	courbe, etiquette, manager = 0,0,0 # Variables contenant les détails des graphiques
+                threading.Thread.__init__(self)
+				
+                self.retrace =  0.1        # Temps en secondes, retrace le graphique à chaque retrace millisecondes
+                self.indice = grandeur     #Indice de la valeur à lire dans data
+				
+				
+                # On définit les paramètres du graphique; une seule figure. Le programme ouvrira une fenêtre différente pour chaque courbe.
+                fig = pylab.figure(self.indice)
+                self.manager = pylab.get_current_fig_manager()  
+     
+                # Initialise quelques variables
+                self.t       = 0.0	       # Temps actuel
+                self.delta_t = 0.1         # Pas de temps
+     
+                #De même que les tableaux, vecteurs temps et valeur de fonction pour graphique
+                self.temps = []
+                self.valeur = []
 
+                # On crée le graphique
+                self.courbe = pylab.plot(self.temps,self.valeur)
+     
+                # Définit le titre et xlabel
+                self.titre     = pylab.title("X en fonction du temps")
+                self.etiquette = pylab.xlabel("Retrace %f " % (self.t) )
+     
+     
+                # Lancer le graphique avec show() et le maintenir actif avec mainloop.
+                self.manager.show()
+                #Tk.mainloop()  # Nécessaire pour garder le programme actif
+   
+     
+        # Cette méthode avance l'horloge, recalcule la fonction et retrace le tout
+     
+        def run(self) :
+                global data
+                while True:
+                    # On calcule le nouveau temps et la valeur de la fonction associée
+                    self.t = self.t + self.delta_t
+     
+                    # Puis on place ces données dans deux tableaux
+                    self.temps.append(self.t)
+     
+                    ### C'est une variable partagé. Il faut lock puis release l'utilisation
+                    with lock:
+                        self.valeur.append(data[self.indice])
+     
+                    # Met à jour les données dans le tableau associé avec le graphique
+                    self.courbe[0].set_data(self.temps,self.valeur)
+     
+                    # On fait de même pour le texte et les axes
+                    self.etiquette.set_text('Retrace au temps %f '%( self.t ))
+     
+                    # Cette commande s'applique au graphique actuel
+                    pylab.axis([min(self.temps)-0.1,max(self.temps)+0.1,min(self.valeur)-0.1,max(self.valeur)+0.1])
+     
+                    # Redraws the figure
+                    self.manager.canvas.draw()
+					
+                    #   Puis, on attend 'retrace' ms avant de relancer run
+                    time.sleep(self.retrace) 
+                    
+     
+    
+    
+### Ici tu as ton main
+### Donne un cas d'utilisation avec lancement du serveur et de ton plot
+### Tu pourras alors tester ton code
+if __name__ == '__main__':
+    data=[0, 2, 3, 5]
+    a = Courbe(2)
+    a.start()
+    for i in xrange(5):
+            data[2] += 1
+            time.sleep(3)
+    # Do things
 
-	# Cette méthode avance l'horloge, recalcule la fonction et retrace le tout
-
-	def run(self) :
-		global data
-		# On calcule le nouveau temps et la valeur de la fonction associée
-		self.t = self.t + self.delta_t
-
-		# Puis on place ces données dans deux tableaux
-		self.temps.append(self.t)
-		self.valeur.append(data)
-
-		# Met à jour les données dans le tableau associé avec le graphique
-		self.courbe[0].set_data(self.temps,self.valeur)
-
-		# On fait de même pour le texte et les axes
-		self.etiquette.set_text('Retrace au temps %f '%( self.t ))
-
-		# Cette commande s'applique au graphique actuel
-		pylab.axis([min(self.temps)-0.1,max(self.temps)+0.1,min(self.valeur)-0.1,max(self.valeur)+0.1])
-
-		# Redraws the figure
-		self.manager.canvas.draw()
-
-		# Puis, on attend 'retrace' ms avant de relancer run
-		self.manager.window.after(self.retrace,self.run)
-
-		
-	def __init__(self, valeur_initiale):
-		global data
-		self.retrace =  100  # Temps en millisecondes 
-
-		# On définit les paramètres du graphique; une seule figure
-		fig = pylab.figure(1)
-		self.manager = pylab.get_current_fig_manager()   
-
-		# Initialise quelques variables
-		self.t       = 0.0
-		self.delta_t = 0.1
-		data = valeur_initiale
-
-		#De même que les tableaux
-		self.temps = []
-		self.valeur = []
-
-		# Ajoute la première valeur à ces tableaux
-		self.temps.append(self.t)
-		self.valeur.append(data)
-
-		# On crée le graphique
-		self.courbe = pylab.plot(self.temps,self.valeur)
-
-		# Définit le titre et xlabel 
-		self.titre     = pylab.title("Sinus en fonction du temps")
-		self.etiquette = pylab.xlabel("Retrace %f " % (self.t) )
-
-		# On trace le tout
-		self.manager.canvas.draw()
-
-		# Puis on lance la méthode qui iterera 
-		self.run()
-
-		# Lancer le graphique avec show() et le maintenir actif avec mainloop.
-		self.manager.show()
-		Tk.mainloop()  # Nécessaire pour garder le programme actif

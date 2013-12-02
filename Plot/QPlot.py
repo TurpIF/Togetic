bd5026506a0f23756435c41fd8dc750098524c2f
TurpIF <pierre.turpin@insa-lyon.fr>
2013-12-02 16:44:17 +0100

    Ajout d'une méthode permettant d'ajouter un point associé à une couleur (par un id)

--- Plot/QPlot.py
+++ Plot/QPlot.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python3
 
 import sys
-from PySide import QtCore, QtGui
+from random import random as rand
+from PySide import QtCore
+from PySide.QtGui import *
 
-class PlotController(QtGui.QWidget):
+class PlotController(QWidget):
   def __init__(self, parent=None):
     super(PlotController, self).__init__(parent)
 
-    self._scene = QtGui.QGraphicsScene()
-    self._view = QtGui.QGraphicsView(self._scene)
+    self._pens = {}
 
-    self._scene.addRect(0, 0, 1, 1)
+    self._scene = QGraphicsScene()
+    self._view = QGraphicsView(self._scene)
 
-    layout = QtGui.QVBoxLayout()
+    layout = QVBoxLayout()
     layout.addWidget(self._view)
     self.setLayout(layout)
 
     self.setWindowTitle("Plot")
 
+  def addPoint(self, id, x, y):
+    if not id in self._pens:
+      color = QColor(rand() * 255, rand() * 255, rand() * 255)
+      self._pens[id] = QPen(color)
+    self._scene.addRect(x, y, 1, 1, self._pens[id])
+
+def testAddPoint(controller):
+  import math
+  f = lambda x, n: 10 * math.sin(x * n)
+  for id in range(5):
+    for x in range(100):
+      controller.addPoint(id, x, f(x, id))
+
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    controller = PlotController()
-    controller.show()
-    sys.exit(app.exec_())
+  app = QApplication(sys.argv)
+  controller = PlotController()
+
+  testAddPoint(controller)
+
+  controller.show()
+  sys.exit(app.exec_())

f03e3d1c3f4c97826d48567bbccc4a026770de6e
TurpIF <pierre.turpin@insa-lyon.fr>
2014-01-12 15:35:31 +0100

    Intégration dy gyro

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,16 +2,87 @@
 #include <HMC5883L.h>
 #include <ADXL345.h>
 
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
+
+int L3G4200D_Address = 105; //I2C address of the L3G4200D
+
+void setupGyro() {
+  setupL3G4200D(2000); // Configure L3G4200  - 250, 500 or 2000 deg/sec
+}
+
+void readingGyro(int * x, int * y, int * z) {
+  byte xMSB = readRegister(L3G4200D_Address, 0x29);
+  byte xLSB = readRegister(L3G4200D_Address, 0x28);
+  *x = ((xMSB << 8) | xLSB);
+
+  byte yMSB = readRegister(L3G4200D_Address, 0x2B);
+  byte yLSB = readRegister(L3G4200D_Address, 0x2A);
+  *y = ((yMSB << 8) | yLSB);
+
+  byte zMSB = readRegister(L3G4200D_Address, 0x2D);
+  byte zLSB = readRegister(L3G4200D_Address, 0x2C);
+  *z = ((zMSB << 8) | zLSB);
+}
+
+int setupL3G4200D(int scale){
+  //From  Jim Lindblom of Sparkfun's code
+
+  // Enable x, y, z and turn off power down:
+  writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+
+  // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
+  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000000);
+
+  // Configure CTRL_REG3 to generate data ready interrupt on INT2
+  // No interrupts used on INT1, if you'd like to configure INT1
+  // or INT2 otherwise, consult the datasheet:
+  writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
+
+  // CTRL_REG4 controls the full-scale range, among other things:
+
+  if(scale == 250) {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00000000);
+  } else if(scale == 500) {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00010000);
+  } else {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00110000);
+  }
+
+  // CTRL_REG5 controls high-pass filtering of outputs, use it
+  // if you'd like:
+  writeRegister(L3G4200D_Address, CTRL_REG5, 0b00000000);
+}
+
+void writeRegister(int deviceAddress, byte address, byte val) {
+    Wire.beginTransmission(deviceAddress); // start transmission to device 
+    Wire.write(address);       // send register address
+    Wire.write(val);         // send value to write
+    Wire.endTransmission();     // end transmission
+}
+
+int readRegister(int deviceAddress, byte address){
+    int v;
+    Wire.beginTransmission(deviceAddress);
+    Wire.write(address); // register to read
+    Wire.endTransmission();
+
+    Wire.requestFrom(deviceAddress, 1); // read a byte
+    while(!Wire.available());
+
+    v = Wire.read();
+    return v;
+}
+
 static HMC5883L compass;
 static ADXL345 accel;
 
 void setupAccel() {
   accel = ADXL345();
   accel.powerOn();
-}
-
-void setupGyro() {
-  // TODO
 }
 
 void setupCompass() {
@@ -30,10 +101,6 @@ void setup() {
 
 void readingAccel(int * x, int * y, int * z) {
   accel.readAccel(x, y, z);
-}
-
-void readingGyro(int * x, int * y, int * z) {
-  // TODO
 }
 
 void readingCompass(int * x, int * y, int * z) {

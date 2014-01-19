eee73653c9bfb3849954eb59860ea147cdb73ff7
mfalce <falce.matthieu@gmail.com>
2014-01-19 18:24:28 +0100

    le plot fonctionne avec les données du filtre :)

--- plot_mat.py
+++ plot_mat.py
@@ -3,12 +3,14 @@
 # datas
 import socket
 from Queue import Queue
-import random
+import threading
+import time
 
 # maths and graph
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import random
 
 
 
@@ -17,7 +19,9 @@ nb_points = 100
 t = [i for i in xrange(nb_points)]
 l_names_subplot_1 = ['accel_x', 'accel_y', 'accel_z']
 l_names_subplot_2 = ['rot_x', 'rot_y', 'rot_z']
-nb_var = len(l_names_subplot_1) + len(l_names_subplot_2)
+nb_var = 1 + len(l_names_subplot_1) + len(l_names_subplot_2)
+queue = Queue()
+
 
 #############################    data management
 def read_data(file_in, queue, nbr_data):
@@ -30,29 +34,33 @@ def read_data(file_in, queue, nbr_data):
                 pass
             else:
                 queue.put(f)
-
+        time.sleep(0.01)
 
 def get_random_data():
     return [random.random()-0.5 for i in xrange(nb_var)]
 
 
 
-#queue = Queue()
-#sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#sock.connect('/tmp/togetic_sortie_filtre')
-#file_sock = sock.makefile('r', bufsize=0)
-
+#############################    socket connection
+print("INIT: socket")
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+sock.connect('/tmp/togetic_sortie_filtre')
+file_sock = sock.makefile('r', bufsize=0)
+thread = threading.Thread(target=read_data, args=(file_sock, queue, nb_var))
+thread.start()
 
 ##############################   matplotlib stuff
 # graphic creation
+print("INIT: matplotlib axes")
 fig, (ax1, ax2) = plt.subplots(2, sharex=True)  #xlim=(0, 100), ylim=(-1, 1))
 
 # axes limits
 ax1.set_xlim((0, 100))
-ax1.set_ylim((-1, 1))
+ax1.set_ylim((-10, 10))
 ax2.set_xlim((0, 100))
-ax2.set_ylim((-1, 1))
+ax2.set_ylim((-10, 10))
 
+print("INIT: plots creation")
 # init plots (create one line object for each data)
 lines = []
 for n in l_names_subplot_1:
@@ -75,27 +83,36 @@ time_text = ax1.text(0.5, 0.9, '', ha='center', va='center',
 # update and init fonction
 
 def animate(i):
-    data = get_random_data()
-    time_text.set_text('time = {}'.format(i % 1000)) # :( doesn't work
-    for l, d in zip(lines, data):
-        tmp = l.get_ydata()  # get the previous values
-        tmp.append(d)  # add the new data
-        if len(tmp) >= len(t):
-            tmp = tmp[1:]  # queue style (first we contruct the queue)
-        l.set_data(t[:len(tmp)], tmp)  # update the plot data
+    data = None
+    if not queue.empty():
+        data = queue.get()
+    print('DRAWING: queue content : {}'.format(data))
 
-    # suz hack lolz (we need a single list of objects)
+    time_text.set_text('time = {}'.format(i % 1000)) # :( doesn't work
+
+    if data is not None:
+        for l, d in zip(lines, data[1:]):
+            tmp = l.get_ydata()  # get the previous values
+            tmp.append(d)  # add the new data
+            if len(tmp) >= len(t):
+                tmp = tmp[1:]  # queue style (first we contruct the queue)
+            l.set_data(t[:len(tmp)], tmp)  # update the plot data
+
+        # suz hack lolz (we need a single list of objects)
+
     return lines + [time_text]
 
 
 #Init only required for blitting to give a clean slate.
 def init():
+    print('DRAWING: init')
     time_text.set_text("")
     # if we put data in the lines, they won't be cleared during the update
     for l in lines:
         l.set_data([], [])
     return lines + [time_text]
 
+print("INIT: animation")
 ani = animation.FuncAnimation(fig, animate, np.arange(1, 1000), init_func=init,
     interval=25, blit=True)
 plt.show()

70062c115acae74fd4b69824ac2df4e53221376f
mfalce <falce.matthieu@gmail.com>
2014-01-19 17:42:04 +0100

    bon le plot est bon... mais sans les données

--- plot_mat.py
+++ plot_mat.py
@@ -15,8 +15,9 @@ from matplotlib import animation
 ##############################   constants
 nb_points = 100
 t = [i for i in xrange(nb_points)]
-l_names = ['accel_x', 'accel_y', 'accel_z', 'rot_x', 'rot_y', 'rot_z']
-
+l_names_subplot_1 = ['accel_x', 'accel_y', 'accel_z']
+l_names_subplot_2 = ['rot_x', 'rot_y', 'rot_z']
+nb_var = len(l_names_subplot_1) + len(l_names_subplot_2)
 
 #############################    data management
 def read_data(file_in, queue, nbr_data):
@@ -32,7 +33,7 @@ def read_data(file_in, queue, nbr_data):
 
 
 def get_random_data():
-    return [random.random()-0.5 for i in xrange(len(l_names))]
+    return [random.random()-0.5 for i in xrange(nb_var)]
 
 
 
@@ -44,20 +45,30 @@ def get_random_data():
 
 ##############################   matplotlib stuff
 # graphic creation
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 100), ylim=(-1, 1))
+fig, (ax1, ax2) = plt.subplots(2, sharex=True)  #xlim=(0, 100), ylim=(-1, 1))
+
+# axes limits
+ax1.set_xlim((0, 100))
+ax1.set_ylim((-1, 1))
+ax2.set_xlim((0, 100))
+ax2.set_ylim((-1, 1))
 
 # init plots (create one line object for each data)
 lines = []
-for n in l_names:
-    line, = ax.plot([], [], label=n)
+for n in l_names_subplot_1:
+    line, = ax1.plot([], [], label=n)
+    lines.append(line)
+for n in l_names_subplot_2:
+    line, = ax2.plot([], [], label=n)
     lines.append(line)
 
 # legends, grid...
-legend = ax.legend(loc='upper left', shadow=True)
-plt.grid()
-time_text = ax.text(0.5, 0.9, '', ha='center', va='center',
-                    transform=ax.transAxes)
+legend1 = ax1.legend(loc='upper left', shadow=True)
+legend2 = ax2.legend(loc='upper left', shadow=True)
+ax1.grid()
+ax2.grid()
+time_text = ax1.text(0.5, 0.9, '', ha='center', va='center',
+                    transform=ax1.transAxes)
 
 
 #############################    animation
@@ -72,15 +83,18 @@ def animate(i):
         if len(tmp) >= len(t):
             tmp = tmp[1:]  # queue style (first we contruct the queue)
         l.set_data(t[:len(tmp)], tmp)  # update the plot data
+
+    # suz hack lolz (we need a single list of objects)
     return lines + [time_text]
 
 
 #Init only required for blitting to give a clean slate.
 def init():
+    time_text.set_text("")
     # if we put data in the lines, they won't be cleared during the update
     for l in lines:
         l.set_data([], [])
-    return lines
+    return lines + [time_text]
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, 1000), init_func=init,
     interval=25, blit=True)

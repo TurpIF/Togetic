a9ae0f557fd798bfbf757f2a019afd2664267fa6
TurpIF <pierre.turpin@insa-lyon.fr>
2014-01-24 15:57:35 +0100

    fichier de lib arduino

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,133 @@
+#include <Wire.h>
+#include <HMC5883L.h>
+#include <ADXL345.h>
+
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
+
+int L3G4200D_Address = 105; //I2C address of the L3G4200D
+
+void setupGyro() {
+  setupL3G4200D(2000); // Configure L3G4200  - 250, 500 or 2000 deg/sec
+}
+
+void readingGyro(int * x, int * y, int * z) {
+  byte xMSB = readRegister(L3G4200D_Address, 0x29);
+  byte xLSB = readRegister(L3G4200D_Address, 0x28);
+  *x = ((xMSB << 8) | xLSB);
+
+  byte yMSB = readRegister(L3G4200D_Address, 0x2B);
+  byte yLSB = readRegister(L3G4200D_Address, 0x2A);
+  *y = ((yMSB << 8) | yLSB);
+
+  byte zMSB = readRegister(L3G4200D_Address, 0x2D);
+  byte zLSB = readRegister(L3G4200D_Address, 0x2C);
+  *z = ((zMSB << 8) | zLSB);
+}
+
+int setupL3G4200D(int scale){
+  //From  Jim Lindblom of Sparkfun's code
+
+  // Enable x, y, z and turn off power down:
+  writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+
+  // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
+  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000000);
+
+  // Configure CTRL_REG3 to generate data ready interrupt on INT2
+  // No interrupts used on INT1, if you'd like to configure INT1
+  // or INT2 otherwise, consult the datasheet:
+  writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
+
+  // CTRL_REG4 controls the full-scale range, among other things:
+
+  if(scale == 250) {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00000000);
+  } else if(scale == 500) {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00010000);
+  } else {
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00110000);
+  }
+
+  // CTRL_REG5 controls high-pass filtering of outputs, use it
+  // if you'd like:
+  writeRegister(L3G4200D_Address, CTRL_REG5, 0b00000000);
+}
+
+void writeRegister(int deviceAddress, byte address, byte val) {
+    Wire.beginTransmission(deviceAddress); // start transmission to device 
+    Wire.write(address);       // send register address
+    Wire.write(val);         // send value to write
+    Wire.endTransmission();     // end transmission
+}
+
+int readRegister(int deviceAddress, byte address){
+    int v;
+    Wire.beginTransmission(deviceAddress);
+    Wire.write(address); // register to read
+    Wire.endTransmission();
+
+    Wire.requestFrom(deviceAddress, 1); // read a byte
+    while(!Wire.available());
+
+    v = Wire.read();
+    return v;
+}
+
+static HMC5883L compass;
+static ADXL345 accel;
+
+void setupAccel() {
+  accel = ADXL345();
+  accel.powerOn();
+  accel.setFullResBit(true);
+}
+
+void setupCompass() {
+  compass = HMC5883L();
+  compass.SetMeasurementMode(Measurement_Continuous);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  setupAccel();
+  setupGyro();
+  setupCompass();
+  delay(1000);
+}
+
+void readingAccel(int * x, int * y, int * z) {
+  accel.readAccel(x, y, z);
+}
+
+void readingCompass(int * x, int * y, int * z) {
+  MagnetometerRaw raw = compass.ReadRawAxis();
+  *x = raw.XAxis;
+  *y = raw.YAxis;
+  *z = raw.ZAxis;
+}
+
+void loop() {
+  char c = '\0';
+  while(c != 'a' && c != 'g' && c != 'c') {
+      c = Serial.read();
+  }
+  int x = 0, y = 0, z = 0;
+  if(c == 'a')
+      readingAccel(&x, &y, &z);
+  else if(c == 'g')
+      readingGyro(&x, &y, &z);
+  else
+      readingCompass(&x, &y, &z);
+  Serial.print(x);
+  Serial.print(" ");
+  Serial.print(y);
+  Serial.print(" ");
+  Serial.println(z);
+}
+
+// vim: set syntax=cpp sw=2 ts=2 et:
